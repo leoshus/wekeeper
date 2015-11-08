8f4668dab38b58b43a07ec5857dda4360598997d
sydsoft <sydhappy123@163.com>
2015-11-08 23:03:42 +0800

    shiro config

--- src/main/java/com/sdw/soft/wekeeper/web/action/LandingController.java
+++ src/main/java/com/sdw/soft/wekeeper/web/action/LandingController.java
@@ -9,16 +9,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sdw.soft.wekeeper.common.utils.VerifyCodeUtil;
-import com.sdw.soft.wekeeper.common.vo.ResultVo;
 import com.sdw.soft.wekeeper.common.vo.SysUser;
 
 /**
@@ -43,7 +52,7 @@ public class LandingController {
 	 * @param response
 	 * @throws IOException 
 	 */
-	@RequestMapping("/fetchVerifyCode")
+	@RequestMapping(value="/fetchVerifyCode",method=RequestMethod.GET)
 	public void fetchVerifyCode(HttpServletRequest request,HttpServletResponse response) throws IOException{
 		response.setHeader("Pragma", "no-cache");
 		response.setHeader("Cache-Control", "no-cache");
@@ -57,24 +66,52 @@ public class LandingController {
 		
 	}
 	
-	@RequestMapping("/check")
-	public @ResponseBody ResultVo checkLogin(SysUser loginUser,HttpServletRequest request){
-		ResultVo result = new ResultVo();
+	@RequestMapping(value="/check",method=RequestMethod.POST)
+	public String checkLogin(SysUser loginUser,@RequestParam("verifyCode") String verifyCode,HttpServletRequest request,ModelMap model){
+		String redirectUrl = "pub/index";
+		logger.info("username={},password={},输入验证码={}",loginUser.getUsername(),loginUser.getPassword(),verifyCode);
+		String sessionVerifyCode = (String) request.getSession().getAttribute("verifyCode");
+		if(StringUtils.isNotBlank(sessionVerifyCode) && !sessionVerifyCode.equals(verifyCode)){
+			model.addAttribute("message","验证码不正确");
+			return redirectUrl;
+		}
 		UsernamePasswordToken token = new UsernamePasswordToken(loginUser.getUsername(), loginUser.getPassword());
+		token.setRememberMe(true);
 		
+		Subject currentUser = SecurityUtils.getSubject();
 		try {
-			SecurityUtils.getSubject().login(token);
+			currentUser.login(token);
 			HttpSession session = request.getSession(true);
 			try {
 				SysUser user = new SysUser();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			
+			redirectUrl = "layout/main";
+		} catch (UnknownAccountException uae) {
+			logger.info("用户[{}] 未知",loginUser.getUsername());
+//			request.setAttribute("message", "未知用户");
+			model.addAttribute("message", "未知用户");
+		}catch (IncorrectCredentialsException ice){
+			logger.info("用户[{}] 输入密码不正确",loginUser.getUsername());
+			model.addAttribute("message", "用户名/密码不正确");
+		}catch(LockedAccountException lae){
+			logger.info("用户[{}] 账户已锁定",loginUser.getUsername());
+			model.addAttribute("message", "账户已锁定");
+		}catch(ExcessiveAttemptsException eae){
+			logger.info("用户[{}] 验证未通过 错误次数过多",loginUser.getUsername());
+			model.addAttribute("message", "用户名/密码验证错误次数过多");
+		}catch(AuthenticationException ae){
+			logger.info("用户[{}]验证未通过 " ,loginUser.getUsername());
+			ae.printStackTrace();
+			model.addAttribute("message", "用户名/密码不正确");
 		}
 		
-		
-		return result;
+		if(currentUser.isAuthenticated()){
+			logger.info("用户[{}]验证通过",loginUser.getUsername());
+		}else{
+			token.clear();
+		}
+		return redirectUrl;
 	}
 }
--- src/main/webapp/WEB-INF/pages/pub/index.jsp
+++ src/main/webapp/WEB-INF/pages/pub/index.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,7 +15,10 @@
 </head>
 <body>
 	<div align="center">
-		<form action="<%= request.getContextPath() %>/landing/check">
+		<c:if test="${not empty message }">
+			<div style="color:red;font-size:22px;">${message }</div>
+		</c:if>
+		<form action="<%= request.getContextPath() %>/landing/check" method="post">
 		<table>
 			<tr>
 				<td>username:</td>
@@ -26,8 +29,8 @@
 				<td><input type="password" name="password" id="password"/></td>
 			</tr>
 			<tr>
-				<td>verifyCode:</td>
 				<td><img id="verifyCode" alt="verifyCode" onclick="reloadVerifyCode()" src="<%=request.getContextPath()%>/landing/fetchVerifyCode"></td>
+				<td><input type="text" name="verifyCode" id="verifyCode"/></td>
 			</tr>
 			<tr>
 				<td colspan="2"><input type="submit" value="submit"></td>
