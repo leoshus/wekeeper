6b87eea0ff804dd85899d1b036256b8d338d25cf
尚银东 <shangyd@shangyindongdeMacBook-Pro.local>
2015-11-09 22:13:04 +0800

    mybatistest

--- src/test/java/com/sdw/soft/demo/mybatis/MybatisTest.java
+++ src/test/java/com/sdw/soft/demo/mybatis/MybatisTest.java
@@ -1,18 +1,24 @@
 package com.sdw.soft.demo.mybatis;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.sdw.soft.wekeeper.common.user.service.UserService;
+import com.sdw.soft.wekeeper.common.user.vo.SysUser;
+import com.sdw.soft.wekeeper.common.user.vo.UserStatus;
 
 /**
  * @author shangyd
  * @date 2015年11月9日 下午7:10:51
  **/
-@ContextConfiguration(locations="classpath*:/context/spring-context.xml, classpath*:/context/spring-profiles.xml")
-@Profile("mysql")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/context/spring-context.xml"})
+@ActiveProfiles("mysql")
 public class MybatisTest {
 
 	@Autowired
@@ -20,6 +26,15 @@ public class MybatisTest {
 	
 	@Test
 	public void test01(){
-		
+		SysUser user = new SysUser();
+		user.setId(1L);
+		user.setUsername("Tom");
+		user.setPassword("123");
+		user.setSalt("hello");
+		user.setUserStatus(UserStatus.normal);
+		user.setAdmin(true);
+		user.setHasdel(false);
+		int num = userService.save(user);
+		System.out.println(num);
 	}
 }
