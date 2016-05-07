5651e6ea3b24c9ad90dcc3ce09e022b9a0bc3521
shangyindong <shangyindong@corp.netease.com>
2016-05-07 17:16:32 +0800

    remove unused file

--- src/main/java/com/sdw/soft/core/mybatis/handler/ValuedEnum.java
+++
@@ -1,9 +0,0 @@
-package com.sdw.soft.core.mybatis.handler;
-/**
- * @author shangyd
- * @date 2015年11月10日 下午2:27:57
- **/
-public interface ValuedEnum {
-
-	int getValue();
-}
--- src/test/java/com/sdw/soft/demo/spi/IDemoService.java
+++
@@ -1,9 +0,0 @@
-package com.sdw.soft.demo.spi;
-/**
- * @author shangyd
- * @date 2016年4月22日 上午11:32:38
- **/
-public interface IDemoService {
-
-	public String sayHello(String message);
-}
