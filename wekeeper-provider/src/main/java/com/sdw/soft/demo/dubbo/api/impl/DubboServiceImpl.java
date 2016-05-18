af93494e4d4c409e786541e951f04076d5380428
sydsoft <sydhappy123@163.com>
2016-05-18 23:20:15 +0800

    test for RpcContext

--- wekeeper-provider/src/main/java/com/sdw/soft/demo/dubbo/api/impl/DubboServiceImpl.java
+++ wekeeper-provider/src/main/java/com/sdw/soft/demo/dubbo/api/impl/DubboServiceImpl.java
@@ -1,7 +1,11 @@
 package com.sdw.soft.demo.dubbo.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.google.common.base.Joiner;
 import com.sdw.soft.demo.dubbo.api.IDubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author shangyd
@@ -10,8 +14,16 @@ import com.sdw.soft.demo.dubbo.api.IDubboService;
 @Service(version = "1.0.0")
 public class DubboServiceImpl implements IDubboService {
 
+	private static final Logger logger = LoggerFactory.getLogger(DubboServiceImpl.class);
+
 	@Override
 	public String sayHello(String message) {
+		boolean isConsumerSide = RpcContext.getContext().isConsumerSide();
+		boolean isProviderSide = RpcContext.getContext().isProviderSide();
+		logger.info(Joiner.on(" ").join(new Object[]{"isProviderSize=",isProviderSide,"isConsumerSide=",isConsumerSide}));
+		String clientIp = RpcContext.getContext().getRemoteHost();//获取调用方IP
+		logger.info("调用方的IP为:{}",clientIp);
+
 		return "Dubbo say:" + message;
 	}
 
